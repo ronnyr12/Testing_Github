41fb4b2a70330ee495ef19440cfdea29cf610010
Ronny <ronny761@hityash.org>
2022-03-20 13:28:45 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    //complete assignment 1
+    public static int  hw1(int num) {
+        //
+        return 0;
+    }
+}
